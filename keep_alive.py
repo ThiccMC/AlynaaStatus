be2c24c652913a82254393b0eb8efcd62c282f62
QuanTrieuPCYT <trieuq4@outlook.com>
2021-09-22 17:34:29 +0700

    keep_alive and use it with uptimerobot

---
+++ keep_alive.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from threading import Thread
+app = Flask('')
+@app.route('/')
+def main():
+    return "oh hey, i'm ready to receive ping yeet"
+def run():
+    app.run(host="0.0.0.0", port=8080)
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
